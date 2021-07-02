bbbbfd515de46ed33b46f98aabf87514d9f17d9e
Kyle Storck <kylsto@microsoft.com>
2021-07-02 09:53:54 -0700

    Fixed packaging of Protobuf dll and updated build pipeline

---
+++ PerfettoCds/WpaPerfettoLauncher.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+REM Launches WPA with Perfetto plugin
+REM Requires WPA be part of PATH. Requires latest version of Microsoft internal WPA (version 10.7.3.2)
+REM Requires that WPA is launched from same location as trace_processor_shell.exe
+
+REM Change directory to location of this batch file
+cd /d "%~dp0"
+
+REM Launch WPA, specifying it to load this Perfetto plugin directory
+wpa -addsearchdir %cd%
