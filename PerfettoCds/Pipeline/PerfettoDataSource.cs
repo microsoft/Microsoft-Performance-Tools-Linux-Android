71921dbab3b310a8ae3226c79d256d92da306f41
Kyle Storck <kylsto@microsoft.com>
2021-07-08 14:14:11 -0700

    Added attribution for Protobuf and Perfetto. Added AboutInfos

--- PerfettoCds/Pipeline/PerfettoDataSource.cs
+++ PerfettoCds/Pipeline/PerfettoDataSource.cs
@@ -26,6 +26,32 @@ namespace PerfettoCds
                                             this.MetadataTables);
         }
 
+        public override CustomDataSourceInfo GetAboutInfo()
+        {
+            return new CustomDataSourceInfo()
+            {
+                ProjectInfo = new ProjectInfo() { Uri = "https://aka.ms/linuxperftools" },
+                CopyrightNotice = "Copyright (C) " + DateTime.UtcNow.Year,
+                AdditionalInformation = new[]
+                {
+                    "Built using Google Perfetto 4\n" +
+                    "Copyright (C) 2020 The Android Open Source Project\n" +
+                    "\n" +
+                    "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                    "you may not use this file except in compliance with the License.\n" +
+                    "You may obtain a copy of the License at\n" +
+                    "\n" +
+                    "http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    "\n" +
+                    "Unless required by applicable law or agreed to in writing, software\n" +
+                    "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                    "See the License for the specific language governing permissions and\n" +
+                    "limitations under the License.\n"
+                },
+            };
+        }
+
         protected override bool IsDataSourceSupportedCore(IDataSource dataSource)
         {
             if (dataSource.IsDirectory())
@@ -60,6 +86,32 @@ namespace PerfettoCds
                                             processorEnvironment,
                                             this.AllTables,
                                             this.MetadataTables);
+        }
+
+        public override CustomDataSourceInfo GetAboutInfo()
+        {
+            return new CustomDataSourceInfo()
+            {
+                ProjectInfo = new ProjectInfo() { Uri = "https://aka.ms/linuxperftools" },
+                CopyrightNotice = "Copyright (C) " + DateTime.UtcNow.Year,
+                AdditionalInformation = new[]
+                {
+                    "Built using Google Perfetto 4\n" +
+                    "Copyright (C) 2020 The Android Open Source Project\n" +
+                    "\n" +
+                    "Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
+                    "you may not use this file except in compliance with the License.\n" +
+                    "You may obtain a copy of the License at\n" +
+                    "\n" +
+                    "http://www.apache.org/licenses/LICENSE-2.0\n" +
+                    "\n" +
+                    "Unless required by applicable law or agreed to in writing, software\n" +
+                    "distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
+                    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
+                    "See the License for the specific language governing permissions and\n" +
+                    "limitations under the License.\n"
+                },
+            };
         }
 
         protected override bool IsDataSourceSupportedCore(IDataSource dataSource)
