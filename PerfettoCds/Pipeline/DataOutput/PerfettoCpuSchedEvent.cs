5b32c5ff0e121eb858dcea515beab1206e460870
yashsharma-msft <76973932+yashsharma-msft@users.noreply.github.com>
2021-10-15 12:39:13 -0700

    Add previous CPU scheduling event info. (#59)
    * Add previous CPU scheduling event info.
    
    * Update comments.

--- PerfettoCds/Pipeline/DataOutput/PerfettoCpuSchedEvent.cs
+++ PerfettoCds/Pipeline/DataOutput/PerfettoCpuSchedEvent.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using Microsoft.Performance.SDK;
+using System;
 using Utilities;
 
 namespace PerfettoCds.Pipeline.DataOutput
@@ -8,8 +9,11 @@ namespace PerfettoCds.Pipeline.DataOutput
     /// <summary>
     /// A CPU scheduled event that displays which process and threads were running on which CPUs at specific times.
     /// </summary>
-    public readonly struct PerfettoCpuSchedEvent
+    public class PerfettoCpuSchedEvent
     {
+        TimestampDelta? waitDuration;
+        TimestampDelta? schedulingLatency;
+
         public string ProcessName { get; }
         public string ThreadName { get; }
         public long Tid { get; }
@@ -19,7 +23,53 @@ namespace PerfettoCds.Pipeline.DataOutput
         public uint Cpu { get; }
         public string EndState { get; }
         public int Priority { get; }
-        public PerfettoCpuWakeEvent? WakeEvent { get; }
+        public PerfettoCpuWakeEvent WakeEvent { get; private set; }
+        public PerfettoCpuSchedEvent PreviousSchedulingEvent { get; private set; }
+
+        public TimestampDelta WaitDuration
+        {
+            get
+            {
+                if (this.waitDuration == null)
+                {
+                    if (this.WakeEvent != null && this.PreviousSchedulingEvent != null)
+                    {
+                        this.waitDuration = this.WakeEvent.Timestamp - this.PreviousSchedulingEvent.EndTimestamp;
+                    }
+                    else
+                    {
+                        this.waitDuration = TimestampDelta.Zero;
+                    }
+                }
+
+                return waitDuration.Value;
+            }
+        }
+
+        public TimestampDelta SchedulingLatency
+        {
+            get
+            {
+                if (this.schedulingLatency == null)
+                {
+                    if (this.WakeEvent != null)
+                    {
+                        this.schedulingLatency = this.StartTimestamp - this.WakeEvent.Timestamp;
+                    }
+                    else if (this.PreviousSchedulingEvent?.EndState == "Runnable")
+                    {
+                        // Thread was already 'Runnable' and hence was ready to run. Scheduling latency because of non-availability of CPU.
+                        this.schedulingLatency = this.StartTimestamp - this.PreviousSchedulingEvent.EndTimestamp;
+                    }
+                    else
+                    {
+                        this.schedulingLatency = TimestampDelta.Zero;
+                    }
+                }
+
+                return schedulingLatency.Value;
+            }
+        }
 
         public PerfettoCpuSchedEvent(string processName,
             string threadName,
@@ -29,8 +79,7 @@ namespace PerfettoCds.Pipeline.DataOutput
             Timestamp endTimestamp,
             uint cpu,
             string endState,
-            int priority,
-            PerfettoCpuWakeEvent? wakeEvent)
+            int priority)
         {
             this.ProcessName = Common.StringIntern(processName);
             this.ThreadName = Common.StringIntern(threadName);
@@ -41,6 +90,15 @@ namespace PerfettoCds.Pipeline.DataOutput
             this.Cpu = cpu;
             this.EndState = endState;
             this.Priority = priority;
+        }
+
+        public void AddPreviousCpuSchedulingEvent(PerfettoCpuSchedEvent previousCpuSchedEvent)
+        {
+            this.PreviousSchedulingEvent = previousCpuSchedEvent;
+        }
+
+        public void AddWakeEvent(PerfettoCpuWakeEvent wakeEvent)
+        {
             this.WakeEvent = wakeEvent;
         }
     }
